3c3e6b2fc9f631592375d1e45b20f61456a7c9a6
huangweiye <huangweiye@emodor.com>
2023-01-17 17:16:17 +0800

    rrr

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           emmmmmmm.........
         </p>
-        <img className="App-link" src="http://www.desktopwallpaperhd.net/wallpapers/9/6/background-images-wallpaper-eagle-animal-91142.jpg" alt="" />
+        <img className="App-logo" src="http://www.desktopwallpaperhd.net/wallpapers/9/6/background-images-wallpaper-eagle-animal-91142.jpg" alt="" />
       </header>
     </div>
   );
